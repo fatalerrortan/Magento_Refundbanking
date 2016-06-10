bc20d1a47d06977f5bc19cc7ac6d63cfca4bc0e1
fatalerrortan <tiemann9898@gmail.com>
2016-06-10 14:49:49 +0200

    R 0.1.3 Fix Show Sepa Button Logic for different Payment Mthod

--- Step_1/app/code/local/Nextorder/Refundbanking/Block/Adminhtml/Sales/Order/Creditmemo/Create/Items.php
+++ Step_1/app/code/local/Nextorder/Refundbanking/Block/Adminhtml/Sales/Order/Creditmemo/Create/Items.php
@@ -48,22 +48,29 @@ class Nextorder_Refundbanking_Block_Adminhtml_Sales_Order_Creditmemo_Create_Item
                 ))
             );
 //            Sepa Button
-            $this->setChild(
-                'submit_button_sepa',
-                $this->getLayout()->createBlock('adminhtml/widget_button')->setData(array(
-                    'label'     => Mage::helper('sales')->__('Erstattung By SEPA'),
-                    'class'     => 'save submit-button',
-                    'onclick'   => 'disableElements(\'submit-button\');submitCreditMemoSepaOffline()',
-                ))
-            );
+            $order = $this->getOrder();
+            $payment_code = $order->getPayment()->getMethodInstance()->getCode();
+            $paymentPools = array('ops_cc','paypal_billing_agreement','paypal_express');
 
-            $customer = Mage::getModel('customer/customer')->load($this->getOrder()->getCustomerId());
-            if(empty($customer->getData('debit_payment_account_iban')) || empty($customer->getData('debit_payment_account_swift'))){
-                Mage::getSingleton('adminhtml/session')->addWarning("Bitte Fragen Sie die Iban und Bic vom Kunden zur SEPA-Gutschrift nach！");
-                $this->customerCheck = 0;
+            if(!in_array($payment_code, $paymentPools)){
+
+                $customer = Mage::getModel('customer/customer')->load($this->getOrder()->getCustomerId());
+                if(!empty($customer->getData('debit_payment_account_iban')) && !empty($customer->getData('debit_payment_account_swift'))){
+
+                    $this->setChild(
+                        'submit_button_sepa',
+                        $this->getLayout()->createBlock('adminhtml/widget_button')->setData(array(
+                            'label'     => Mage::helper('sales')->__('Erstattung By SEPA'),
+                            'class'     => 'save submit-button',
+                            'onclick'   => 'disableElements(\'submit-button\');submitCreditMemoSepaOffline()',
+                        ))
+                    );
+
+                }else{
+                    Mage::getSingleton('adminhtml/session')->addWarning("Bitte Fragen Sie die Iban und Bic vom Kunden zur SEPA-Gutschrift nach！");
+                    $this->customerCheck = 0;
+                }
             }
-
-
         }
 
         return parent::_prepareLayout();
